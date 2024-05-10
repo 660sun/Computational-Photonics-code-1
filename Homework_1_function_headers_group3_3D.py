4fd6254a404c70df7e8eed27cc452b9c961a018d
Yucheng Sun <ycsun2002@gmail.com>
2024-05-10 15:32:18 +0200

    Yucheng Sun_08-05-2024_3D_v2

--- Homework_1_function_headers_group3_3D.py
+++ Homework_1_function_headers_group3_3D.py
@@ -31,9 +31,9 @@ def guided_modes_2D(prm, k0, h, numb):
         Field distributions of the guided eigenmodes
     """
     dt = np.common_type(np.array([prm]))
-    M  = np.zeros((len(prm), len(prm)), dtype = dt)
-    for i  in range(len(prm)):
-        M[i][i] =  -4/(h**2) + (k0**2) * prm[i][i]
+    M  = np.zeros((np.size(prm), np.size(prm)), dtype = dt)
+    for i  in range(np.size(prm)):
+        M[i][i] =  -4/(h**2) + (k0**2) * prm.flatten()[i]
         if i > 0 & i % np.size(prm,1) != 0:
             M[i][i-1] = 1/(h**2)
         if i < len(prm) - 1 & i % np.size(prm,1) != np.size(prm,1) - 1:
@@ -43,12 +43,16 @@ def guided_modes_2D(prm, k0, h, numb):
         if i < len(prm) - np.size(prm,1):
             M[i][i+np.size(prm,1)] = 1/(h**2)
     M  = (1/(k0**2)) * M
-    eff_eps, guided = sps.linalg.eigs(M, k = numb)
+    eff_eps, guided = sps.linalg.eigs(M, k = numb, which = 'LR')
+    min_val = prm.min()
+    max_val = prm.max()
+    while abs(eff_eps[0]) < min_val or abs(eff_eps[0]) > max_val:
+        eff_eps, guided = eigs(M, k=1, which='LR')
     return eff_eps, guided
 
 # Define the parameters
-grid_size     = 120
-number_points = 301
+grid_size     = 60
+number_points = 61
 h             = grid_size/(number_points - 1)
 lam           = 0.78
 k0            = 2*np.pi/lam
@@ -63,9 +67,11 @@ numb          = 1
 
 # Compute the eigenvalues and eigenvectors
 eff_eps, guided = guided_modes_2D(prm, k0, h, numb)
+# print(eff_eps)
+# print(guided)
 
 X, Y = XX, YY
-Z = np.real(guided[0].reshape((number_points, number_points)))
+Z = np.real(guided.flatten().reshape((number_points, number_points)))
 
 # Plot the eigenmode
 fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
